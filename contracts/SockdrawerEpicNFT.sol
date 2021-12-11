4b30469d51beb4a85e5c755a103cd2597813e24a
Joby Ingram-Dodd <joby@protonmail.com>
2021-12-11 20:12:08 +0000

    First commit

---
+++ contracts/SockdrawerEpicNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract SockdrawerEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Shit the bed!");
+    }
+}
